edcf82f6ac7b59d59a2c57d992f74036d0748687
Alex <aironer@yandex.ru>
2023-02-02 03:25:05 +0300

    load hw 4

---
+++ hw4.js
@@ -0,0 +1,50 @@
+/*
+
+//z 1
+let i = 1;
+
+let n = Number(2);
+
+while (i <= n) {
+    alert('Привет!');
+    i++;
+}
+
+
+//z 2
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+
+//z 3 
+
+for (let i = 7; i <= 22; i++) {
+    console.log(i);
+}
+
+
+//z 4
+let obj = {
+    'Коля': '200',
+    'Вася': '300',
+    'Петя': '400',
+}
+
+for (let key in obj) {
+    alert(key + ' - зарплата ' +  obj[key] + '$');
+    break;
+}
+
+
+ 
+//z 5
+for (let n = 1000; n > 50; n++) {
+    if ( n /= 2) {
+     alert(n);}
+     }
+
+*/
+
+
